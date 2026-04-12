039d689e144fa5a0311078a7740dbd844bd5aa7a
Saba <sabaphruidze01@gmail.com>
2026-04-12 17:51:13 +0400

    feat(sessionType):starting session type writing

--- src/pages/Courses/components/CourseDetailRight.tsx
+++ src/pages/Courses/components/CourseDetailRight.tsx
@@ -3,13 +3,14 @@ import THREE from "../../../assets/icons/courses/Icon Set=Three.svg";
 import DESKTOP from "../../../assets/icons/courses/Icon Set=Desktop.svg";
 
 import TimeSlot from "./TimeSlot";
+import SessionType from "./SessionType";
 
 const CourseDetailRight = () => {
   return (
     <div className="mt-[130px] w-[530px] flex flex-col gap-[32px]">
       <WeeklySchedule />
       <TimeSlot />
-      <div id="session-type"></div>
+      <SessionType />
       <div id="total-price"></div>
     </div>
   );
---
+++ src/pages/Courses/components/SessionType.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SessionType = () => {
+  return <div>SessionType</div>;
+};
+
+export default SessionType;
